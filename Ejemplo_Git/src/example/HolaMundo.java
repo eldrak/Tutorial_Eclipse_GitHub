f892c2988a59e5ef59b1f59630911eb01f9adc3b
eldrak <danielgibarra@hotmail.com>
2014-10-29 14:12:05 -0600

    prueba fetch
    haber si se baja

--- Ejemplo_Git/src/example/HolaMundo.java
+++ Ejemplo_Git/src/example/HolaMundo.java
@@ -1,16 +1,9 @@
 package example;
-
 public class HolaMundo 
 {
-
 	public static void main(String[] args) 
 	{
 		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");		
 	}
 
 }
